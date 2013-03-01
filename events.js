5de63477a0a412e82cf4d759a50e75f45f971727
jsDotCr <rocco@js.cr>
2013-03-01 11:36:53 +0100

    Adding pubsub first draft implementation.

---
+++ events.js
@@ -0,0 +1,31 @@
+var onGoingNotification = {};
+
+function notify(name, data){
+	if (!name || typeof(name)!=='string'){
+		throw new TypeError("Invalid event name '" + name);
+	}
+	if (!data || typeof(data)!=='object'){
+		throw new TypeError("Invalid event data '" + data);
+	}
+
+	if (onGoingNotification[name] instanceof Array) {
+		var listeners = [].concat(onGoingNotification[name]);
+		for (var i = 0, len = listeners.length; i < len; i++) {
+			listeners[i].call(this, data);
+		}
+	}
+}
+
+function subscribe(name, handler){
+	if (!name || typeof(name)!=='string'){
+		throw new TypeError("Invalid event name '" + name);
+	}
+	if (!handler || typeof(handler)!=='function'){
+		throw new TypeError("Invalid event handler '" + handler);
+	}
+
+	if (typeof onGoingNotification[name] === "undefined") {
+		onGoingNotification[name] = [];
+	}
+	onGoingNotification[name].push(handler);
+}
