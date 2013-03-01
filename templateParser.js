8f90cfc3b03d92f5fcdf30db436337a463d193ec
Stefano Sergio <stefano@appsfuel.com>
2013-03-01 11:30:09 +0100

    modified template Parser and alpha.html

--- templateParser.js
+++ templateParser.js
@@ -1,25 +1,31 @@
 var regExpToMatchName = new RegExp("data-rf-(\\w*)");
 var regExpToMatchValue = new RegExp("\\{\\{(.*)\\}\\}");
+var symbolTable = [];
+var referenceCounter = 0;
+//TODO Clonare la root dentro un fragment
+//TODO Trasformare in hash con un ID = chiave primaria (generare id seq. se non definito nel dom)
 
-function _parseAttributes(stuffToBeReplaced, node, regExpToMatchName, regExpToMatchValue,
-	 					/* privates */ nodeAttributes, matchedElms, attribute, attributeName, attributeValue){
+function _parseAttributes(symbolTable, node, regExpToMatchName, regExpToMatchValue, refId,
+	 					/* privates */ nodeAttributes, matchedElms, attribute, attributeName, attributeValue) {
 	nodeAttributes = node.attributes;
-	for (var i = 0; attribute = nodeAttributes[i]; i++){
+	for (var i = 0; attribute = nodeAttributes[i]; i++) {
 		attributeName = attribute.name;
 		attributeValue = attribute.value;
-		if (!attribute.specified){ // prevents parsing default and unspecified attributes
+		if (!attribute.specified) { // prevents parsing default and unspecified attributes
 			continue;
 		}
-		if (matchedElms = attributeName.match(regExpToMatchName)){
-			stuffToBeReplaced.push({
+		if (matchedElms = attributeName.match(regExpToMatchName)) {
+
+			symbolTable.push({
 				domElement: node,
 				action: matchedElms[1],
 				attribute: attribute,
 				attributeName: attributeName,
 				attributeValue: attributeValue
 			});
-		} else if (regExpToMatchValue.test(attributeValue)){
-			stuffToBeReplaced.push({
+		}
+		else if (regExpToMatchValue.test(attributeValue)) {
+			symbolTable.push({
 				domElement: node,
 				action: "replaceAttributeValue",
 				attribute: attribute,
@@ -29,20 +35,22 @@ function _parseAttributes(stuffToBeReplaced, node, regExpToMatchName, regExpToMa
 		}
 	}
 }
-function DOMParser(stuffToBeReplaced, node,
-				   /* privates */ nodeValue){
+function DOMParser(node,
+				   /* privates */ nodeValue, matchedElms) {
 	nodeValue = node.nodeValue;
 	switch (node.nodeType){
 		case 1:
-			_parseAttributes(stuffToBeReplaced, node, regExpToMatchName, regExpToMatchValue);
-			for (var i=0, childElm; childElm = node.childNodes[i++];){
-				domParser(stuffToBeReplaced, childElm);
+			_parseAttributes(symbolTable, node, regExpToMatchName, regExpToMatchValue);
+			for (var i=0, childElm; childElm = node.childNodes[i++];) {
+				DOMParser(childElm);
 			}
 			break;
 		case 3:
-			if (regExpToMatchValue.test(nodeValue)){
-				stuffToBeReplaced.push({
-					domElement: node,
+			//if (regExpToMatchValue.test(nodeValue)) {
+			if (matchedElms = nodeValue.match(regExpToMatchValue)) {
+				symbolTable.push({
+					textNode: node,
+					domElement: node.parentElement,
 					action: "replaceText",
 					text: nodeValue
 				});
@@ -50,6 +58,11 @@ function DOMParser(stuffToBeReplaced, node,
 			break;
 	}
 }
-// Usage: DOMParser(Array, DOMElement);
+
+function getSymbolTable() {
+	return symbolTable;
+}
+
+// Usage: DOMParser(DOMElement);
 // Array: Contains elements to be replaced by template renderer
 // DOMElement: Root DOM element to start from
