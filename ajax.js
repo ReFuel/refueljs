81240e8ac8442a75e33ec4ef91eda0a38f68e629
jsDotCr <rocco@js.cr>
2013-03-15 16:58:48 +0100

    Adding timeout && auto-retry implementation as well.

--- ajax.js
+++ ajax.js
@@ -1,5 +1,6 @@
 define(function() {
 	var ajaxCounter = 0;
+	var timedOutCalls = {};
 
 	function setProvider(){
 		var XMLHttpRequest;
@@ -31,6 +32,30 @@ define(function() {
 		return urlElm.protocol + "//" + urlElm.host === window.location.protocol + "//" + window.location.host;
 	}
 
+	function killAjaxCall(xhr, url, options){
+		xhr.onreadystatechange = null;
+		xhr.abort();
+		if (!timedOutCalls[url]){
+			timedOutCalls[url] = 1;
+		} else {
+			timedOutCalls[url]++;
+		}
+
+		var resp = {
+			url: url,
+			responseText: "",
+			responseJSON: {}
+		};
+		if (options.timeout){
+			options.timeout(resp, 0, xhr);
+		} else if (timedOutCalls[url] <= 2) {
+			options.msTimeout *= 1.5;
+			ajax(url, options);
+		} else {
+			options.ko(resp, 0, xhr);
+		}
+	}
+
 	function ajax(url, options){
 		var xhr = setProvider();
 		var method = options.method ? options.method : "GET";
@@ -40,13 +65,20 @@ define(function() {
 			// @TODO - maybe - CORS requests
 			return false;
 		}
+		options.msTimeout = options.msTimeout || 100;
+		var timeout = setTimeout(function(xhr, url, options){
+			return function(){
+				killAjaxCall(xhr, url, options);
+			}
+		}(xhr, url, options), options.msTimeout);
 
 		xhr.onreadystatechange = function() {
-			var status, ct, resp;
+			var status, ct, resp = {};
 			if (xhr.readyState === 1){
 				ajaxCounter++;
 			}
 			if (xhr.readyState === 4){
+				clearTimeout(timeout);
 				ajaxCounter--;
 				try {
 					status = xhr.status;
@@ -54,12 +86,11 @@ define(function() {
 					status = 0;
 				}
 				ct = xhr.getResponseHeader("Content-Type");
-				if (ct === 'application/json' && xhr.responseText){
-					resp = JSON.parse(xhr.responseText) || {};
-				} else if (ct === 'application/xml') {
-					resp = xhr.responseXML;
-				} else {
-					resp = xhr.responseText;
+				resp.url = url;
+				resp.responseXML = xhr.responseXML;
+				resp.responseText = xhr.responseText;
+				if (ct === 'application/json' && resp.responseText){
+					resp.responseJSON = JSON.parse(resp.responseText) || {};
 				}
 				if (status >= 200 && status < 400 || status === 1224){
 					options.ok(resp, status, xhr);
@@ -67,7 +98,7 @@ define(function() {
 					options.ko(resp, status, xhr);
 				} else if (status === 0){
 					// @TODO timeout implementation!
-					options.timeout(xhr);
+					options.timeout(resp, 0, xhr);
 				}
 			}
 		};
