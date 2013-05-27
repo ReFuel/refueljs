f9cd634823a992b5e2ccd3de7e35f8b4be82ebaf
Stefano Sergio <stefano@appsfuel.com>
2013-05-27 10:18:08 +0000

    Last ALPHA commit. Goodbye alpha, hello beta

--- Refuel.js
+++ Refuel.js
@@ -174,7 +174,7 @@
 		if (startupModule) baseConfig.paths[startupModule] = startupPath+'/'+startupModule;
 
 		Refuel.config = Refuel.mix(baseConfig, Refuel.config || {});
-      	requirejs.config(Refuel.config);
+      	require.config(Refuel.config);
 		startupRequirements = [startupModule];
       	if (!window.Path) startupRequirements.push('path.min');
       	if (!window.Hammer) startupRequirements.push('hammer.min');
